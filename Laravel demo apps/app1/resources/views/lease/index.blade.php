9a2d8d6dbce5ffe3c8b6627fd129b7aa92c3ce06
bpropelled <abgquickbase@gmail.com>
2018-08-10 22:15:37 -0400

    added a view for the demo app

--- Laravel demo apps/app1/resources/views/lease/index.blade.php
+++ Laravel demo apps/app1/resources/views/lease/index.blade.php
@@ -6,10 +6,13 @@
             <div class="col-lg-12">
                 <h1> Leasees</h1>
                 @foreach ($leases as $l)
+                {{-- Leasee Name --}}
                 <h2>{{ $l->name }}</h2>
+                {{-- Company Name --}}
                 <h3>{{ $l->comp }}</h3>
+                {{-- Name of the Office Type --}}
                 <p>{{ $office->name  }}</p>
-                    
+
                 <hr>
                 @endforeach
 
